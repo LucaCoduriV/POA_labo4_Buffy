7af15c59c689d27c31b57260683ca30a5a544d58
chloefont <cfontaine@citycable.ch>
2022-05-19 15:48:57 +0200

    controller

---
+++ src/Controller.h
@@ -0,0 +1,14 @@
+//
+// Created by cfont on 19.05.2022.
+//
+
+#ifndef POA_LABO4_BUFFY_CONTROLLER_H
+#define POA_LABO4_BUFFY_CONTROLLER_H
+
+
+class Controller {
+
+};
+
+
+#endif //POA_LABO4_BUFFY_CONTROLLER_H
