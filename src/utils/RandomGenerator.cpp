758c78f8d9bb6661978563652c6396dcc79f72f4
cfont <cfontaine@citycable.ch>
2022-06-08 08:27:20 +0200

    stats ok

--- src/utils/RandomGenerator.cpp
+++ src/utils/RandomGenerator.cpp
@@ -2,12 +2,27 @@
 // Created by cfont on 20.05.2022.
 //
 
+#include <ctime>
 #include "RandomGenerator.hpp"
+#include <random>
 
-static std::random_device rd;
-static std::mt19937 gen(rd());
+using namespace std;
+
+//static random_device rd;
+//static mt19937 gen(rd());
+
 
 int createRandomNb(int min, int max) {
-   std::uniform_int_distribution<int> distr(min, max);
-   return distr(gen);
+   //static default_random_engine eng{rd()};
+   //uniform_int_distribution<int> distr(min, max);
+   static bool first = true;
+
+   if (first) {
+      srand(time(0));
+      first = false;
+   }
+
+   return min + (rand() % ( max - min + 1 ) );
+
+   //return distr(eng);
 }
