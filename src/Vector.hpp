7b748fdd3d5d610431df45f56d9db3843430e3c7
Luca Coduri <coduri.vitor@gmail.com>
2022-06-09 20:00:59 +0200

    comments

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -1,20 +1,37 @@
-//
-// Created by cfont on 19.05.2022.
-//
-
 #ifndef POA_LABO4_BUFFY_VECTOR_HPP
 #define POA_LABO4_BUFFY_VECTOR_HPP
 
 #include <iostream>
-
+/**
+ * This class stores a vector in two dimensions.
+ * It makes able to make operations between vectors with ease.
+ */
 class Vector {
 public:
    friend std::ostream& operator<<(std::ostream& os, const Vector& vector);
 
+   /**
+    * Adds two vectors.
+    * @param v1
+    * @param v2
+    * @return
+    */
    friend Vector operator+(const Vector& v1, const Vector& v2);
 
+   /**
+    * Multiplies a vector with an other.
+    * @param v1
+    * @param mult
+    * @return
+    */
    friend Vector operator*(const Vector& v1, int mult);
 
+   /**
+    * Multiplies a vector with a number.
+    * @param mult
+    * @param v1
+    * @return
+    */
    friend Vector operator*(int mult, const Vector& v1);
 
    Vector(int x, int y);
@@ -23,6 +40,11 @@ public:
 
    Vector& operator=(const Vector& other);
 
+   /**
+    * Calculates the distance between two vectors.
+    * @param other
+    * @return the distance
+    */
    double distance(const Vector& other) const;
 
    Vector direction(const Vector& to) const;
