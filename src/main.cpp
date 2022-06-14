01f1cbeeac57b8a1f9094eca5d74879c439707b7
Luca Coduri <coduri.vitor@gmail.com>
2022-06-14 17:17:43 +0200

    memory leak fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 
 using namespace std;
 
+const int FIELD_HEIGHT = 10;
+const int FIELD_WIDTH = 10;
+
 int main() {
 
    try {
+      auto displayer =
+         make_shared<ColoredConsoleDisplayer>(FIELD_HEIGHT,FIELD_WIDTH);
+
       //TODO récupérer inputs utilisateurs
-      Controller controller(50, 50, 10, 20, new ColoredConsoleDisplayer(50, 50));
+      Controller controller(FIELD_WIDTH, FIELD_HEIGHT, 10, 20, displayer.get());
    } catch (const invalid_argument& e) {
       cout << e.what();
    }
